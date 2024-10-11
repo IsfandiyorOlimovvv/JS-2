2c21ac4271207ea6b57dcd7683c1b33fca3b9cde
IsfandiyorOlimovvv <shoxagaming@gmail.com>
2024-10-11 23:50:55 +0500

    js-2 lesson

---
+++ app.js
@@ -0,0 +1,97 @@
+// 1
+
+// let L = 100;
+// let toliqMetrlar = parseInt(L / 100);
+// console.log(toliqMetrlar);
+
+// 2
+
+// let M = 1000;
+// let toliqTonnalar = parseInt(M / 1);
+// console.log(toliqTonnalar);
+
+
+/////////////          7        /////////////////////
+
+// let number = prompt("Ikki xonali son kiriting:");
+// number = parseInt(number);
+
+// let onliklar = parseInt(number / 10);
+// let birlar = number % 10;
+
+// alert("onnliklar xonasidegi raqam: " + onliklar + "birlar xonasidegi raqam: " + birlar);
+
+
+// 9
+// let number = prompt("Uch xonali son kiriting:");
+// number = parseInt(number);
+
+// let yuzlar = parseInt(number / 100);
+
+// alert("Yuzlar xonasidaegi raqam: " + yuzlar);
+
+// 11
+// let number = prompt("uch xonali son kiriting:");
+// number = parseInt(number);
+
+// let yuzlar = parseInt(number / 100);
+// let onliklar = parseInt((number % 100) / 10);
+// let birlar = number % 10;
+
+// let yigindi = yuzlar + onliklar + birlar;
+
+// alert("raqamlar yigindisi: " + yigindi);
+
+// 8
+// let number = prompt("Ikki xonali son kiriting:");
+// number = parseInt(number);
+
+// let onliklar = parseInt(number / 10);
+// let birlar = number % 10;
+
+// let yangiSon = (birlar * 10) + onliklar
+
+// alert("raqamlar o'rni almashtirilgan son: " + yangiSon);
+
+// 10
+// let number = prompt("Uch xonali son kiriting:");
+// number = parseInt(number);
+
+// let birliklar = number % 10;
+// let onliklar = Math.floor((number % 100) / 10);
+
+// alert("birliklar xonasidagi raqam: " + birliklar + "onliklar xonasidagi raqam: " + onliklar);
+
+// 12
+// let number = prompt("Uch xonali son kiriting:");
+// number = parseInt(number);
+
+// let yuzlar = parseInt(number / 100);
+// let onliklar = parseInt((number % 100) / 10);
+// let birliklar = number % 10;
+
+// let teskariSon = (birliklar * 100) + (onliklar * 10) + yuzlar;
+
+// alert("teskari tartibda hosil bolgan son: " + teskariSon); 
+
+
+// 13
+// let number = prompt("uch xonali son kiriting:");
+// number = parseInt(number);
+
+// let yuzlar = parseInt(number / 100);
+// let onliklarBirliklar = number % 100;
+
+// let yangiSon = (onliklarBirliklar * 10) + yuzlar;
+
+// alert("hosil bolgan son: " + yangiSon);
+
+
+// 6
+// let number = prompt("ikki xonali son kiriting:");
+// number = parseInt(number);
+
+// let onliklar = parseInt(number / 10);
+// let birliklar = number % 10;
+
+// alert("Onliklar xonasidagi raqam: " + onliklar + "birliklar xonasidagi raqam: " + birliklar);
